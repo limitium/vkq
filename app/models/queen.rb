f0d146664532beb7d13b39326072b48e8a23a914
Sergey Belov <limitium@gmail.com>
2011-06-19 21:39:16 +0400

    model added

---
+++ app/models/queen.rb
@@ -0,0 +1,20 @@
+class Queen
+  include MongoMapper::Document
+
+  key :uid, Integer
+  key :first_name, String
+  key :last_name, String
+  key :domain, String
+  key :sex, String
+  key :bdate, String
+  key :city, String
+  key :country, String
+  key :photo, String
+  key :photo_medium, String
+  key :photo_big, String
+  key :university_name, String
+  key :faculty, String
+  key :faculty_name, String
+  key :graduation, String
+
+end
