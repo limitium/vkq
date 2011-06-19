bb564ca5bebc59e5a756c426c7703444eeaba608
Sergey Belov <limitium@gmail.com>
2011-06-20 02:00:45 +0400

    api testing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,33 @@
-window.name = 'fXD';
 
 VK.init(function() {
+    console.log('vk inited');
+    $(document).ready(function() {
+        console.log('app start');
+        getFriends();
+    });
+}, function() {
+    alert('Ошибка инициализации JS VK API');
+});
+
+function getFriends() {
+    console.log('getFrieds')
+    VK.api('friends.get', {fields:"uid, first_name, last_name, nickname, sex, photo_medium"}, function(data) {
+        console.log('f result')
+        if (data.response) {
+            var html_res = '';
+            var f_cnt = data.response.length;
+            for (var i = 0; i < f_cnt; i++)
+                html_res += '<img src="' + data.response[i].photo_medium + '" align="left">';
+
+            //html_res +=''+data.response[i].last_name+'';
+            //html_res +='';
+        }
+
+        console.log(html_res);
+    });
+
+}
+function qwe() {
     // API initialization succeeded
     // Your code here
     console.log(arguments);
@@ -11,4 +38,4 @@ VK.init(function() {
     VK.api('getUserSettings', function(data) {
         console.log(data)
     });
-});
+}
