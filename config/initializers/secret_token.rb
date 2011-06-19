4c413f7acb3b018927ec240758771c7bf4e95c06
Sergey Belov <limitium@gmail.com>
2011-06-19 17:16:44 +0400

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vkq::Application.config.secret_token = '96bd46d48c522275863671f516666c2a2c0e5dcf4ea94ab503f1b7cc2219167475f60352f96451e7b6681cb5d15262f681736a938408a6b5a7328ee631f978dd'
