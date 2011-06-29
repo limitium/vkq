867b14eac9b02074367bd88430a16491a407fcc8
limitium <limitium@gmail.com>
2011-06-29 13:05:49 +0400

    profile data fetch

--- public/javascripts/vkq.js
+++ public/javascripts/vkq.js
@@ -27,12 +27,9 @@ VKQ = {
 			// Code in VKScript lang
 			var code =
 			'var profile = API.getProfiles({"uids": ' + VK.params.viewer_id + ', ' + fields_param + '})[0];' +
-			'var country_name = "";' +
-			'if(profile.country){' +
-			'country_name = API.getCountryById({"cids":profile.country})[0].name'+
-			'}' +
-			//'var city_name = profile.city ? API.getCityById({"cids":profile.city})[0].name : ""' +			
-			'return {"profile":profile,"country_name":country_name,"city_name":123};';
+			'var country_name =  API.getCountryById({"cids":profile.country})@.name'+
+			'var city_name =  API.getCityById({"cids":profile.city})@.name'+
+			'return {"profile":profile,"country_name":country_name,"city_name":city_name};';
 
 
 			VK.api('execute', {'code': code}, function(data) {
