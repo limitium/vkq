d0d2d63cec2731ecd1ec3d04b05605bb4a9c6aab
limitium <limitium@gmail.com>
2011-06-30 11:06:47 +0400

    js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,17 @@
-VKQ.run(function() {
 
+$(document).ready(function() {
+ 	$('.rating_list div.name .button_01').click(function(){
+    	console.log(this)
+	});
+});
+                
+VKQ.run(function() {
+	
+        
+	
     console.log(VK);
     VKQ.updateProfile();
+	
 });
+
+
