fbecbf298fb65a14dd4ae76fb63ea7aca59dfeb7
limitium <limitium@gmail.com>
2011-07-01 10:53:13 +0400

    css and vote fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
  		var but = $(this);
  		var queenLi = but.parents('li');
  		var queen = queenLi.attr('queen');
- 		var val = but.hasClass("button_up") ? 1 : -1;
+ 		var val = but.hasClass("rating_up") ? 1 : -1;
     	
  		VKQ.vote(queen, val, function(){
  			var rating = queenLi.children('div.rating');
