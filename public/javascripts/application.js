3d2833f9737d1626b3ce2d3442ea14538127bcb2
limitium <limitium@gmail.com>
2011-07-06 16:44:28 +0400

    show queen

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,15 @@
 app = {
     initHandlers: function() {
         $('.filter').click(app.onFilterClick);
-        $('.rating_list div.name button').click(app.onVoteClick);
+        $('button.vote_button').click(app.onVoteClick);
     },
     onVoteClick: function() {
-        var but = $(this);
-        var queenLi = but.parents('li');
-        var queen = queenLi.attr('queen');
-        var val = but.hasClass("button_up") ? 1 : -1;
+        var but = $(this);        
+        var queen = but.attr('queen');
+        var val = but.hasClass("rating_up") ? 1 : -1;
 
         VKQ.vote(queen, val, function() {
-            var rating = queenLi.children('div.rating');
+            var rating = $('.rating_'+queen);
             rating.html(parseInt(rating.html()) + val);
         });
     },
