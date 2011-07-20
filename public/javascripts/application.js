beb77a883b325c7d0a0024eba34afc201fbc2a75
Sergey Belov <limitiun@gmail.com>
2011-07-21 01:32:26 +0400

    update error

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -42,7 +42,7 @@ VKQ.run(server.api_id, function() {
     if (server.update_profile) {
         VKQ.updateProfile(function(data) {
             console.log(".name_" + data.profile.uid);
-            console.log($(".name_" + data.profile.uid);
+            console.log($(".name_" + data.profile.uid));
             $(".name_" + data.profile.uid).html(data.profile.first_name + " " + data.profile.last_name);
             $.each(['photo','photo_rec','photo_big','photo_medium_rec'], function(photo) {
                 $("." + photo + "_" + data.profile.uid).attr("src", data.profile[photo]);
