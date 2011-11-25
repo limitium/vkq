9138e18192b467ca529cee8e56ddda97244aa666
limitium <limitium@gmail.com>
2011-11-25 14:01:25 +0300

    achives removed

--- app/models/achive.rb
+++
@@ -1,7 +0,0 @@
-class Achive
-  include Mongoid::Document
-  field :name, :type => String
-  field :description, :type => String
-  field :icon, :type => String
-  field :users_have, :type => Integer
-end
