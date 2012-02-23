ac65b1be41fd5e99242285b87da202f0996d4dae
Sergey Belov <limitiun@gmail.com>
2012-02-24 00:21:12 +0300

    settings

--- public/javascripts/vkq.js
+++ public/javascripts/vkq.js
@@ -121,8 +121,9 @@ VKQ = {
     getFriends: function(cb){
         console.log();
 
-         VK.api("friends.get", cb);
-         VK.api("friends.get",{}, cb);
+         VK.api("friends.get",{
+             fields: "uid, first_name, last_name, nickname, photo_rec"
+         }, cb);
     },
     setLocation: function(location){
         VK.callMethod("setLocation",location);
